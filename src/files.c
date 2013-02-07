2e1c7a68388523056bba994ef37aeafef9d1b37d
Phil Gengler <phil@pgengler.net>
2013-02-07 11:00:53 -0500

    Clean up warnings with -pedantic option
    Original nano commit: http://svn.savannah.gnu.org/viewvc?view=rev&root=nano&revision=4555
    Original nano commit message:
    Clean-up warnings with -pedantic.

--- src/files.c
+++ src/files.c
@@ -1780,7 +1780,6 @@ bool write_file(const char *name, FILE *f_open, bool tmp, append_type
 	/* The actual file, realname, we are writing to. */
     char *tempname = NULL;
 	/* The temp file name we write to on prepend. */
-    int backup_cflags;
 
     assert(name != NULL);
 
@@ -1843,6 +1842,7 @@ bool write_file(const char *name, FILE *f_open, bool tmp, append_type
 	char *backupname;
 	struct utimbuf filetime;
 	int copy_status;
+	int backup_cflags;
 
 	/* Save the original file's access and modification times. */
 	filetime.actime = openfile->current_stat->st_atime;
