70cf3da0d691b2b6d4096b28fb57201d75420aa9
Phil Gengler <phil@pgengler.net>
2014-03-28 09:44:42 -0400

    Fix compilation with --disable-utf8

--- src/pinot.cpp
+++ src/pinot.cpp
@@ -2342,11 +2342,14 @@ int main(int argc, char **argv)
 
 	/* If whitespace wasn't specified, set its default value. */
 	if (whitespace == NULL) {
+#ifdef ENABLE_UTF8
 		if (using_utf8()) {
 			whitespace = mallocstrcpy(NULL, "»·");
 			whitespace_len[0] = 2;
 			whitespace_len[1] = 2;
-		} else {
+		} else
+#endif
+		{
 			whitespace = mallocstrcpy(NULL, ">.");
 			whitespace_len[0] = 1;
 			whitespace_len[1] = 1;
