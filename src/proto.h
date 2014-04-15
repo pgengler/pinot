bac682a9bcfba5d00a39f90e65ea8c4225ad2f3c
Phil Gengler <phil@pgengler.net>
2014-04-15 11:22:46 -0400

    Remove unused add_unicode_digit() and get_unicode_kbinput() functions

--- src/proto.h
+++ src/proto.h
@@ -496,10 +496,6 @@ void dump_filestruct_reverse(void);
 
 /* All functions in winio.c. */
 Key get_kbinput(WINDOW *win);
-#ifdef ENABLE_UTF8
-long add_unicode_digit(int kbinput, long factor, long *uni);
-long get_unicode_kbinput(int kbinput);
-#endif
 int get_control_kbinput(int kbinput);
 std::string get_verbatim_kbinput(WINDOW *win);
 const sc *get_shortcut(int menu, Key& kbinput);
